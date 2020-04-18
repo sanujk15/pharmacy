81492d3633d409fce4d8c849c47ca6e4519d6157
sanuj kalgutkar <sanujkalgutkar@gmail.com>
2020-04-18 15:59:35 +0100

    second change

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "pharmacy_db";
+$servername = "us-cdbr-iron-east-01.cleardb.net";
+$username = "b4540e0f2b47a1";
+$password = "dc5d7a30";
+$dbname = "heroku_5259b59daae6cf4";
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
 if (mysqli_connect_errno()) {
 printf("Connect failed: %s\n", mysqli_connect_error());

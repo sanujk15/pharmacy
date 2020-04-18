14a960b1dfd42119ce7b0933c6c32476f16a43a9
sanuj kalgutkar <sanujkalgutkar@gmail.com>
2020-04-18 18:58:40 +0100

    10th change

--- fetch_data.php
+++ fetch_data.php
@@ -10,7 +10,17 @@
 				$search_query = $_POST["query"];
 			}
 
-			include_once("db_connect.php");
+			//include_once("db_connect.php");
+
+			$servername = "us-cdbr-iron-east-01.cleardb.net";
+			$username = "b4540e0f2b47a1";
+			$password = "dc5d7a30";
+			$dbname = "heroku_5259b59daae6cf4";
+			$conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
+			if (mysqli_connect_errno()) {
+			printf("Connect failed: %s\n", mysqli_connect_error());
+			exit();
+			}
 			
 	if(isset($_POST["action"]))
  {
